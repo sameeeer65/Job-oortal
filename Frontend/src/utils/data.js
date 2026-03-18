dc834de7a8ecbb7e03c378e4789ec25806fbfc37
Khan sameer <sameerahmed2852@gmail.com>
2026-03-18 07:31:20 +0530

    update

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
 export const USER_API_ENDPOINT = "https://job-oortal-4.onrender.com/api/users";
-export const JOB_API_ENDPOINT = "https://job-oortal-4.onrender.com/api/job";
+export const JOB_API_ENDPOINT = "https://job-oortal-4.onrender.comapi/job";
 export const APPLICATION_API_ENDPOINT = "https://job-oortal-4.onrender.com/api/application";
 export const COMPANY_API_ENDPOINT = "https://job-oortal-4.onrender.com/api/company";
